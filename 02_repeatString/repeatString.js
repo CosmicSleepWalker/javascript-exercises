dc0ac59165100974bc3c92c24138a24cb33f61cd
CosmicSleepWalker <dev.Amadeush@gmail.com>
2024-04-15 02:48:37 -0500

    Complete 02 javascript exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = (value, num) => {
+    let repeat = "";
+    if(num < 0){
+        return "ERROR";
+    }
+    for(let i = 0; i < num; i++){
+        repeat += value;
+    }
+    return repeat;
 };
 
 // Do not edit below this line
