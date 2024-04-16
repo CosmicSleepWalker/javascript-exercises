2c7292e8327ffda9e8f73a1ed43f3ecbd788a563
CosmicSleepWalker <dev.Amadeush@gmail.com>
2024-04-16 01:02:03 -0500

    Made a small change to the loop noticed my index variable wasnt being incremented and was the num instead

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -8,8 +8,8 @@ const sumAll = function(num1, num2) {
         num1 = temp;
     }
     let sum = 0;
-    for(let i = num1; num1 <= num2; num1++){
-        sum+= num1;
+    for(let i = num1; i <= num2; i++){
+        sum+= i;
     }
     return sum;
 };
