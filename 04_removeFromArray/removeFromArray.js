bcf9109f4de70e517a37fb56e56016c703458757
CosmicSleepWalker <dev.Amadeush@gmail.com>
2024-04-15 21:46:27 -0500

    Complete 04 Javascript exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
-
+// paramaters( array, an array of numbers to remove which acts as arguments)
+const removeFromArray = function(arr, ...numbersToRemove) {
+    for(let j = 0; j < arr.length; j++){
+        for(let i = 0; i < numbersToRemove.length; i++){
+            let index = arr.indexOf(numbersToRemove[i]);
+            if(index >= 0){
+                arr.splice(index, 1);
+            }
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
