8fbacb88f128d52a05b47b74d5a135667a2d74ed
CosmicSleepWalker <dev.amadeush@gmail.com>
2024-04-25 21:56:37 -0500

    Complete 10th exercise of javascript

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    num = parseInt(num);
+    if(num === 0){
+        return 0;
+    }else if(num < 0){
+        return "OOPS";
+    }
+    let counter = 1;
+    let num1 = 1;
+    let num2 = 1;
+    while(counter < num){
+        let temp = num2;
+        num2 = num1 + num2;
+        num1 = temp;
+        counter++;
+    }
+    return num1;
 };
 
 // Do not edit below this line
