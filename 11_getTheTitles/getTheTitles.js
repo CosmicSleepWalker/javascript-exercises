17c29158e9a5ae22af6085400a81cf4154a52350
CosmicSleepWalker <dev.amadeush@gmail.com>
2024-04-25 22:10:51 -0500

    Complete 11th exercise for javascript

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,10 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    let arr = [];
+    console.log(books[0].title);
+    books.forEach((book, index) => {
+        arr[index] = book.title;
+    });
+    return arr;
 };
 
 // Do not edit below this line
