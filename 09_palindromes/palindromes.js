f7e28a641c764cc9fcf6e7081bc5c67b0009a3f4
CosmicSleepWalker <dev.amadeush@gmail.com>
2024-04-25 20:02:07 -0500

    completed 09 exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
-
+const palindromes = function (phrase) {
+    const punctuation = /[.,/! ]/g;
+    const removedPunctuations = phrase.replace(punctuation, '').toUpperCase();
+    let i = 0;
+    let j = removedPunctuations.length - 1;
+    let result = true;
+    while(i < j){
+        if(removedPunctuations.charAt(i) !== removedPunctuations.charAt(j)){
+            result = false;
+            break;
+        }
+        i++;
+        j--;
+    }
+    return result;
 };
 
 // Do not edit below this line
