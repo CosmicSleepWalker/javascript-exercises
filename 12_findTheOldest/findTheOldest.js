b10314407f4ed77d11e380450616494ac0f10e43
CosmicSleepWalker <dev.amadeush@gmail.com>
2024-04-26 22:11:13 -0500

    Complete 12th exercise for javascript

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let oldest = people[0];
 
+    for(let i = 0; i < people.length; i++){
+        let date = new Date();
+        let currentOldestAge = oldest.yearOfDeath === undefined? date.getFullYear() - oldest.yearOfBirth : oldest.yearOfDeath - oldest.yearOfBirth;
+        let currentAge = people[i].yearOfDeath - people[i].yearOfBirth;
+        if(people[i].yearOfDeath === undefined){
+            let livingAge = date.getFullYear() - people[i].yearOfBirth;
+            if( livingAge > currentOldestAge){
+                oldest = people[i];
+            }
+        } else if(currentOldestAge < currentAge){
+            oldest = people[i];
+        }
+
+    }
+    return oldest;
 };
 
 // Do not edit below this line
